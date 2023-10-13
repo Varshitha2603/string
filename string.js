b7763b591e19d1d1e0b2b557fbf7157ba0e19149
DCKAP <varshithaksdckap@gmail.com>
2023-10-13 15:50:57 +0530

    string

---
+++ string.js
@@ -0,0 +1,62 @@
+//-----------------1. Write a JavaScript function to check whether an 'input' is a string or not------------------
+
+
+// let a = prompt('Enter the string or number')
+// let c = a.trim()
+// if(c){
+//     let parse = parseInt(c)
+//     if(parse%2<=0|parse%2>=0){
+//         console.log('number')
+//     }else{
+//         console.log('string')
+//     }
+// }else{
+//     console.log('invalid output')
+// }
+
+
+//-----------------2. Write a JS function to check whether a string is blank or not------------------------------
+
+
+// let a = prompt("Enter a value");
+// let b=a.trim();
+// if(b.length==0)
+// {
+//     console.log("True");
+// }
+// else
+// {
+//     console.log("False");
+// }
+
+
+//------------------3. Write a JS function to split a string and convert it into an array of words------------------
+
+
+// let a = prompt("Enter the one sentences");
+// let b = a.split(" ");
+// console.log(b);
+
+
+//-----------------4. Write a JS function to extract a specified number of characters from a string------------------
+
+
+// let a = prompt("Enter the one sentences");
+// let b = a.substr(0,5);
+// console.log(b);
+
+
+//----------------5. to lowerCase() to toUpperCase()------------------------------------------------------------------
+
+
+// let strings = prompt('Enter a string to change' );
+// let output = strings.split('');
+// let ans = '';
+// for(i in output){
+//     if(output[i].match(/[a-z]/)){
+//        ans += output[i].toUpperCase()
+//     }else{
+//         ans += output[i].toLowerCase()
+//     }
+// }
+// document.write(ans);
